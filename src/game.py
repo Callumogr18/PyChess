6925077f188c4283300f10f29584bb3a986fc9aa
Callumogr18 <callum.ryan.2702@gmail.com>
2024-06-25 23:36:02 +0100

    Dragging of pieces now implemented

--- src/game.py
+++ src/game.py
@@ -1,9 +1,13 @@
 import pygame
 from const import *
+from board import Board
+from dragger import Dragger
+
 
 class Game:
     def __init__(self):
-        pass
+        self.board = Board()
+        self.dragger = Dragger()
 
     # Show methods
     def show_bg(self, surface):
@@ -15,3 +19,26 @@ class Game:
                     color = (119, 154, 88) # Dark green
                 rect = (col * SQSIZE, row * SQSIZE, WIDTH, HEIGHT)
                 pygame.draw.rect(surface, color, rect)
+
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # Check for piece on the board
+                """
+                1. Checking if square has a piece
+                2. Saving that piece in a variable
+                3. Taking the image of the piece
+                4. Center the image
+                5. The texture_rect is going to be
+                   the image we created, but centered
+                6. Blit the image inside texture_rect
+                """
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+                    if piece is not self.dragger.piece:
+                        piece.set_texture(size=80)
+                        img = pygame.image.load(piece.texture)
+                        # Center img on x-axis
+                        img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+                        piece.texture_rect = img.get_rect(center=img_center)
+                        surface.blit(img, piece.texture_rect)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import sys
 from const import *
 from game import Game
 
+
 class Main:
 
     def __init__(self):
@@ -14,14 +15,47 @@ class Main:
     def mainloop(self):
         screen = self.screen
         game = self.game
+        board = self.game.board
+        dragger = self.game.dragger
 
         while True:
             # Displays the chessboard
             game.show_bg(screen)
+            game.show_pieces(screen)  # Show all pieces except the dragged one
+
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+
+                # Click
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
+
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+
+                # Mouse motion
+                elif event.type == pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_pieces(screen)
+                        dragger.update_blit(screen)
+
+                # Click release
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    dragger.undrag_piece()
+
+                # Quit game
+                elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+            if dragger.dragging:
+                dragger.update_blit(screen)  # Ensure dragged piece is drawn on top
 
             pygame.display.update()
 
