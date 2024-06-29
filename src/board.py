d06b549b375f55177d8ec6fe81b404dc457c322b
Callumogr18 <callum.ryan.2702@gmail.com>
2024-06-29 23:21:15 +0100

    Every piece besides king now able to navigate board. Rook, Bishop and Queen sharing the one method for the straight line moves on the board to make code cleaner

--- src/board.py
+++ src/board.py
@@ -20,6 +20,39 @@ class Board:
         :param col: The column on the board
         """
 
+        def pawn_moves():
+            steps = 1 if piece.moved else 2
+            start = row + piece.dir
+            end = row + (piece.dir * (1 + steps))
+
+            # Vertical moves
+            for move_row in range(start, end, piece.dir):
+                if Square.in_range(move_row):
+                    if self.squares[move_row][col].isempty():
+                        # Create initial and final move squares
+                        initial = Square(row, col)
+                        final = Square(move_row, col)
+                        # Create a new move
+                        move = Move(initial, final)
+                        piece.add_move(move)
+                    # Blocked
+                    else:
+                        break
+                # Not in range
+                else:
+                    break
+
+            # Diagonal moves
+            possible_move_row = row + piece.dir
+            possible_move_cols = [col-1, col+1]
+            for possible_move_col in possible_move_cols:
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row][possible_move_col].has_enemy_piece(piece.color):
+                        initial = Square(row, col)
+                        final = Square(possible_move_row, possible_move_col)
+                        move = Move(initial, final)
+                        piece.add_move(move)
+
         def knight_moves():
             # 8 Possible moves
             possible_moves = [
@@ -36,7 +69,7 @@ class Board:
             for possible_move in possible_moves:
                 possible_move_row, possible_move_col = possible_move
                 if Square.in_range(possible_move_row, possible_move_col):
-                    if self.squares[possible_move_row][possible_move_col].isempty_or_rival(piece.color):
+                    if self.squares[possible_move_row][possible_move_col].isempty_or_enemy(piece.color):
                         # Create squares of the new move
                         initial = Square(row, col)
                         final = Square(possible_move_row, possible_move_col)
@@ -44,23 +77,66 @@ class Board:
                         move = Move(initial, final)
                         piece.add_move(move)
 
-        if isinstance(piece, Pawn):
+        def straightline_moves(incrs):
+            for incr in incrs:
+                row_incr, col_incr = incr
+                possible_move_row = row + row_incr
+                possible_move_col = col + col_incr
+
+                while True:
+                    if Square.in_range(possible_move_row, possible_move_col):
+                        initial = Square(row, col)
+                        final = Square(possible_move_row, possible_move_col)
+                        move = Move(initial, final)
+
+                        # Empty case = continue looping
+                        if self.squares[possible_move_row][possible_move_col].isempty():
+                            piece.add_move(move)
+
+                        # Has enemy piece = add move + break
+                        if self.squares[possible_move_row][possible_move_col].has_enemy_piece(piece.color):
+                            piece.add_move(move)
+                            break
+
+                        # Has team piece = break
+                        if self.squares[possible_move_row][possible_move_col].has_team_piece(piece.color):
+                            break
+
+                    else: break
+
+                    possible_move_row = possible_move_row + row_incr
+                    possible_move_col = possible_move_col + col_incr
+
+        def king_moves():
             pass
 
-        elif isinstance(piece, Knight):
-            knight_moves()
+        if isinstance(piece, Pawn): pawn_moves()
+        elif isinstance(piece, Knight): knight_moves()
+        elif isinstance(piece, Bishop): straightline_moves([
+            (-1, 1), # up-right
+            (-1, -1), # up-left
+            (1, 1), # down-right
+            (1, -1) # down-left
+        ])
+        elif isinstance(piece, Rook): straightline_moves([
+            (-1, 0), # up
+            (0, 1), # left
+            (1, 0), # down
+            (0, -1) # left
+        ])
 
-        elif isinstance(piece, Bishop):
-            pass
-
-        elif isinstance(piece, Rook):
-            pass
-
-        elif isinstance(piece, Queen):
-            pass
-
-        elif isinstance(piece, King):
-            pass
+        # Note: Queen is a combination of the Bishop and Rook
+        elif isinstance(piece, Queen): straightline_moves([
+            (-1, 1),  # up-right
+            (-1, -1),  # up-left
+            (1, 1),  # down-right
+            (1, -1),  # down-left
+            (-1, 0),  # up
+            (0, 1),  # left
+            (1, 0),  # down
+            (0, -1)  # left
+        ])
+        elif isinstance(piece, King): straightline_moves()
 
     def _create(self):
         # Creating a list of 8 zeroes for each column
